b7ab5a27609642d0edecf577437f810117000a43
Brian Rose <stetsentech@gmail.com>
2022-08-22 07:04:46 -0400

    Corrects typo in form button ID

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ class App {
   constructor() {
     this.$form = document.querySelector('#form');
     this.$noteTitle = document.querySelector('#note-title');
-    this.$formButtons = document.querySelector("#form_buttons");
+    this.$formButtons = document.querySelector("#form-buttons");
 
     this.addEventListener();
   }
