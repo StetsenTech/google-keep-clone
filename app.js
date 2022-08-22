aace1734b87e7ef2600565306934f81006a15df7
Brian Rose <stetsentech@gmail.com>
2022-08-22 14:35:58 -0400

    Adds ability to display notes and open/close form

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ class App {
   constructor() {
     this.notes = [];
 
+    this.$placeholder = document.querySelector('#placeholder');
+    this.$notes = document.querySelector("#notes");
     this.$form = document.querySelector('#form');
     this.$noteTitle = document.querySelector('#note-title');
     this.$noteText = document.querySelector('#note-text');
     this.$formButtons = document.querySelector("#form-buttons");
+    this.$formCloseButton = document.querySelector("#form-close-button");
 
     this.addEventListener();
   }
@@ -24,13 +27,24 @@ class App {
           this.addNote({title, text});
         }
     });
+
+    this.$formCloseButton.addEventListener('click', event => {
+        event.stopPropagation();
+        this.closeForm();
+    });
   }
 
   handleFormClick(event) {
       const isFormClicked = this.$form.contains(event.target);
 
+      const title = this.$noteTitle.value;
+      const text = this.$noteText.value;
+      const hasNote = title || text;
+
       if(isFormClicked) {
         this.openForm();
+      } else if (hasNote) {
+        this.addNote({title, text});
       } else {
         this.closeForm();
       }
@@ -47,17 +61,36 @@ class App {
     this.$form.classList.remove('form-open');
     this.$noteTitle.style.display = "none";
     this.$formButtons.style.display = "none";
+    this.$noteTitle.value = "";
+    this.$noteText.value = "";
   }
   
-  addNote(note) {
+  addNote({title, text}) {
     const newNote = {
-      title: note.title,
-      text: note.text,
+      title: title,
+      text: text,
       color: 'white',
       id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
     };
     this.notes = [...this.notes, newNote];
-    console.log(this.notes);
+    this.displayNotes();
+    this.closeForm();
+  }
+
+  displayNotes() {
+    const hasNotes = this.notes.length > 0;
+    this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
+
+    this.$notes.innerHTML = this.notes.map(note => `
+      <div style="background: ${note.color};" class="note">
+        <div class"${note.title && 'note-title'}">${note.title}</div>
+        <div class="note-text">${note.text}</div>
+        <div class="toolbar">
+          <img class="toolbar-color" src="https://icon.now.sh/palette">
+          <img class="toolbar-delete" src="https://icon.now.sh/delete">
+        </div>
+      </div>
+    `).join("");
   }
 }
 
